d1bc035b5817a82cb1eb7615314fb74012d91c22
DrozdovMK <drozdovmk@ioes.ru>
2024-09-27 16:42:44 +0300

    Добавлена папка notebooks

--- scripts/embedding_model.py
+++ scripts/embedding_model.py
@@ -1,4 +1,5 @@
 import tensorflow_hub as hub
+import tensorflow_text
 class universal_sentence_encoder():
     def __init__(self):
         self.model = hub.load("https://www.kaggle.com/models/google/universal-sentence-encoder/TensorFlow2/multilingual/2")
